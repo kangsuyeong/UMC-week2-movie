628e30c27b55ba85c35ce44c5beaebee6b2f5d38
kangsuyeong <ekdh16@naver.com>
2024-05-22 13:44:39 +0900

    🎨 Login page 수정
    Login page 수정

--- src/page/LoginPage.jsx
+++ src/page/LoginPage.jsx
@@ -1,17 +1,18 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { styled } from "styled-components";
 
 const BackGround = styled.div`
   background-color: black;
-  height: 100vh;
+  height: 92vh;
   color: white;
   display: flex;
   justify-content: center;
 `;
 const SignUpForm = styled.form`
-  width: 400px;
+  width: 450px;
   margin: 40px;
+  margin-top: 100px;
 `;
 const Title = styled.div`
   font-size: 25px;
@@ -45,8 +46,68 @@ const SubmitBtn = styled.button`
   border: 2px solid white;
 `;
 
+const ErrorMessage = styled.div`
+  color: red;
+  font-size: 15px;
+`;
+
 const LoginPage = () => {
   const { pathname } = useLocation();
+
+  //id
+  const [id, setID] = useState("");
+  const [idError, setIDError] = useState("");
+  const [idAvailable, setIDAvailable] = useState(false);
+
+  //비밀번호
+  const [password, setPassword] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [passwordAvailable, setPasswordAvailable] = useState(false);
+
+  // id Handler
+  const onChangeIDHandler = (e) => {
+    const idValue = e.target.value;
+    setID(idValue);
+    idCheckHandler(idValue);
+  };
+
+  // password Handler
+  const onChangePasswordHandler = (e) => {
+    const passwordValue = e.target.value;
+    setPassword(passwordValue);
+    passwordCheckHandler(passwordValue);
+  };
+
+  const passwordCheckHandler = async (password) => {
+    const passwordForm =
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{4,12}$/; //최소 4자리 이상 최대 12자리 영어, 숫자, 특수문자 모두조합
+    if (password == "") {
+      setPasswordError("비밀번호을 입력해주세요.");
+      setPasswordAvailable(false);
+    } else if (!passwordForm.test(password)) {
+      setPasswordError(
+        "비밀번호는 4-12자의 영소문자, 숫자, 특수문자를 모두 조합해서 입력해주세요."
+      );
+      setPasswordAvailable(false);
+    } else {
+      setPasswordError("");
+      setPasswordAvailable(true);
+    }
+  };
+
+  const idCheckHandler = async (id) => {
+    const idForm = /^[a-z]+[a-z0-9]{5,19}$/g;
+    if (id == "") {
+      setIDError("아이디을 입력해주세요.");
+      setIDAvailable(false);
+    } else if (!idForm.test(id)) {
+      setIDError("아이디는 6-20자의 영소문자, 숫자만 입력이 가능합니다.");
+      setIDAvailable(false);
+    } else {
+      setIDError("");
+      setIDAvailable(true);
+    }
+  };
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
@@ -56,16 +117,29 @@ const LoginPage = () => {
         <Title>로그인 페이지</Title>
         <div>
           <InputArea>
-            <label htmlFor="name">이름</label>
+            <label htmlFor="id">아이디</label>
             <div>
-              <InputBox type="text" placeholder="이름" id="name" />
+              <InputBox
+                type="text"
+                placeholder="아이디"
+                id="id"
+                onChange={onChangeIDHandler}
+              />
             </div>
+            {idError && <ErrorMessage>{idError}</ErrorMessage>}
           </InputArea>
+
           <InputArea>
             <label htmlFor="password">비밀번호</label>
             <div>
-              <InputBox type="password" placeholder="비밀번호" id="password" />
+              <InputBox
+                type="password"
+                placeholder="비밀번호"
+                id="password"
+                onChange={onChangePasswordHandler}
+              />
             </div>
+            {passwordError && <ErrorMessage>{passwordError}</ErrorMessage>}
           </InputArea>
         </div>
 
